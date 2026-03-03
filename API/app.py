2edb86812705ab4b6decb7e8cd79db56228af770
diamantinojunior204-coder <diamantinojunior204@gmail.com>
2026-03-03 08:42:22 -0300

    Update app.py

--- API/app.py
+++ API/app.py
@@ -348,5 +348,10 @@ def api_cartas():
         processar_aposta(session["user_id"], "cartas", aposta, calcular)
     )
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
